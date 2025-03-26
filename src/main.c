2b8245d537ab0e8f51970530ef68f1e934ba1b7c
jakeec <jake.ethan.carrington@gmail.com>
2025-03-26 21:00:30 +0000

    Add pausing.

--- src/main.c
+++ src/main.c
@@ -32,10 +32,21 @@ int main(void) {
   bool fall_faster = false;
 
   bool running = true;
+  bool paused = false;
   while (running) {
     if (WindowShouldClose()) {
       running = false;
       break;
+    }
+
+    if (paused) {
+      if (IsKeyPressed(KEY_P)) {
+        paused = false;
+      }
+    } else {
+      if (IsKeyPressed(KEY_P)) {
+        paused = true;
+      }
     }
 
     if (IsKeyDown(KEY_RIGHT)) {
@@ -48,47 +59,50 @@ int main(void) {
 
     fall_faster = IsKeyDown(KEY_DOWN);
 
-    // Run interval updates
-    double now = GetTime();
-    if (now - last_move_update > move_speed) {
-      last_move_update = now;
+    if (!paused) {
+      double now = GetTime();
+      if (now - last_move_update > move_speed) {
+        last_move_update = now;
 
-      if (move_right) {
-        active.x++;
-        move_right = false;
+        if (move_right) {
+          active.x++;
+          move_right = false;
+        }
+
+        if (move_left) {
+          active.x--;
+          move_left = false;
+        }
+
+        if (active.x > COLS - 1) {
+          active.x = COLS - 1;
+        }
+
+        if (active.x < 0) {
+          active.x = 0;
+        }
       }
 
-      if (move_left) {
-        active.x--;
-        move_left = false;
+      // Run interval updates
+      double final_fall_speed = fall_faster ? fall_speed / 2 : fall_speed;
+      if (now - last_fall_update > final_fall_speed) {
+        last_fall_update = now;
+
+        // Collision
+        if (active.y >= ROWS - 1) {
+          active.y = ROWS - 1;
+          grid[active.y][active.x] = active.type;
+          active = next;
+          next = tetromino_spawn();
+        }
+
+        active.y++;
       }
-
-      if (active.x > COLS - 1) {
-        active.x = COLS - 1;
-      }
-
-      if (active.x < 0) {
-        active.x = 0;
-      }
-    }
-
-    double final_fall_speed = fall_faster ? fall_speed / 2 : fall_speed;
-    if (now - last_fall_update > final_fall_speed) {
-      last_fall_update = now;
-
-      // Collision
-      if (active.y >= ROWS - 1) {
-        active.y = ROWS - 1;
-        grid[active.y][active.x] = active.type;
-        active = next;
-        next = tetromino_spawn();
-      }
-
-      active.y++;
     }
 
     BeginDrawing();
-    ClearBackground(RAYWHITE);
+#define BACKGROUND CLITERAL(Color){0x2E, 0x30, 0x3E, 0xFF} // 2E303E
+    ClearBackground(BACKGROUND);
 
     // Draw the grid
     DrawRectangleLines(PADDING, PADDING, CELLSIZE * COLS, CELLSIZE * ROWS,
--- src/tetromino.h
+++ src/tetromino.h
@@ -4,6 +4,15 @@
 #include <raylib.h>
 #include <stdlib.h>
 
+
+#define ICOLOR CLITERAL(Color){0x6C, 0x6F, 0x93, 0xFF} // BLUE
+#define OCOLOR CLITERAL(Color){0xFA, 0xC2, 0x9A, 0xFF} // YELLOW
+#define TCOLOR CLITERAL(Color){0xBA, 0x77, 0xDB, 0xFF} // MAGENTA
+#define SCOLOR CLITERAL(Color){0xE9, 0x56, 0x78, 0xFF} // RED
+#define ZCOLOR CLITERAL(Color){0x1E, 0xB9, 0x80, 0xFF} // GREEN
+#define JCOLOR CLITERAL(Color){0xEE, 0x64, 0xAE, 0xFF} // PINK
+#define LCOLOR CLITERAL(Color){0xF0, 0x93, 0x83, 0xFF} // ORANGE
+
 typedef struct {
   // Tetromino types: I O T S Z J L
   char type;
@@ -12,40 +21,32 @@ typedef struct {
   int x, y;
 } Tetromino;
 
+char _types[7] = {'I', 'O', 'T', 'S', 'Z', 'J', 'L'};
+
 Tetromino tetromino_spawn() {
-  char types[7] = {'I', 'O', 'T', 'S', 'Z', 'J', 'L'};
-  char type = types[rand() % 7];
+  char type = _types[rand() % 7];
   return (Tetromino){type, 'U', 4, 0};
 }
 
 Color tetromino_get_color(char tetromino_type) {
-  Color color = BLUE;
-
   switch (tetromino_type) {
   case 'I':
-    color = BLUE;
-    break;
+    return ICOLOR;
   case 'O':
-    color = YELLOW;
-    break;
+    return OCOLOR;
   case 'T':
-    color = MAGENTA;
-    break;
+    return TCOLOR;
   case 'S':
-    color = RED;
-    break;
+    return SCOLOR;
   case 'Z':
-    color = GREEN;
-    break;
+    return ZCOLOR;
   case 'J':
-    color = PINK;
-    break;
+    return JCOLOR;
   case 'L':
-    color = ORANGE;
-    break;
+    return LCOLOR;
+  default:
+    return ICOLOR;
   }
-
-  return color;
 }
 
 #endif //_TETROMINO_H
